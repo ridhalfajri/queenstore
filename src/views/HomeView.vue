a2ad8b68173fa0b76a0161a290a559e4ba6488c0
ridhalfajri <ridhalfajri00@gmail.com>
2022-07-15 16:21:24 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderQueen />
+    <HeroQueen />
+    <WomenBannerQueen />
+    <InstaQueen />
+    <PartnerQueen />
+    <FooterQueen />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderQueen from '@/components/HeaderQueen.vue'
+import HeroQueen from '@/components/HeroQueen.vue'
+import WomenBannerQueen from '@/components/WomenBannerQueen.vue'
+import InstaQueen from '@/components/InstaQueen.vue'
+import PartnerQueen from '@/components/PartnerQueen.vue'
+import FooterQueen from '@/components/FooterQueen.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+    HeaderQueen,
+    HeroQueen,
+    WomenBannerQueen,
+    InstaQueen,
+    PartnerQueen,
+    FooterQueen
+  }
+}
+</script>
